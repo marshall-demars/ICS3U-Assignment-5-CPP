c498a1763ef770886b284dbda77349d5973e3004
Marshall Demars <marshall.demars@mths.ca>
2022-11-20 22:27:56 +0000

    fix assignment 5

--- guessingGame.cpp
+++ guessingGame.cpp
@@ -2,41 +2,38 @@
 
 // Created by: Marshall Demars
 // Created on: Nov 2022
-// This program calculates factorials
+// This program calculates multiplication tables for user input
 
 #include <iostream>
 
 int main() {
-    // This program calculates factorials
+    // This program calculates multiplication tables for user input
 
-    int integerAsInt;
-    int counter = 1;
-    int factorialProduct = 1;
-    std::string integerAsString;
+    int inputAsInt;
+    int counter = 0;
+    int product = 1;
+    int TABLE_END = 10;
+    std::string inputAsString;
 
     // Input
     std::cout << "Enter a positive number: ";
-    std::cin >> integerAsString;
-    std::cout << "" << std::endl;
+    std::cin >> inputAsString;
 
     // Process and Output
     try {
-        integerAsInt = std::stoi(integerAsString);
-
-        if (integerAsInt < 0) {
-            std::cout << "Please enter a positive number." << std::endl;
+        inputAsInt = std::stoi(inputAsString);
+        if (inputAsInt < 0) {
+            std::cout << "\nPlease enter a positive number." << std::endl;
         } else {
-            (integerAsInt > 0);
-            do {
-                factorialProduct = factorialProduct * counter;
-                counter = counter + 1;
-            } while (counter <= integerAsInt);
-            std::cout << "The product of all positive numbers from 1 to "
-                      << integerAsString << " is "
-                      << factorialProduct << "." << std::endl;
+            } while (counter < TABLE_END); {
+            counter = counter + 1;
+            product = inputAsInt * counter;
+            std::cout << "\n"
+                      << inputAsString << " x " << counter << " = "
+                      << product << "." << std::endl;
         }
     } catch (std::invalid_argument) {
-        std::cout << "That was not a valid input." << std::endl;
+        std::cout << "Invalid input." << std::endl;
     }
 
     std::cout << ("\nDone.") << std::endl;
